9b7b1bd60bfe0f751352d2ca2db540ab578e7652
RaphaelS1 <raphaelsonabend@gmail.com>
2021-12-17 09:48:48 +0000

    add

--- code/fairness_grid.R
+++ code/fairness_grid.R
@@ -11,19 +11,30 @@ eval_groupwise(lrn("surv.coxph")$train(train), test)
 # Define grid to evaluate
 grd = expand.grid(
     trt_effect = seq(0, 2, length.out = 3),
-    maj_mean = seq(0.05, 5, length.out = 5),
-    mnt_mean = seq(0.05, 5, length.out = 5),
-    maj_sd = seq(0, 5, length.out = 5),
-    mnt_sd = seq(0, 5, length.out = 5),
-    p_white = c(0.1, 0.3, 0.5),
-    p_trt = c(0.1, 0.3, 0.5),
+    maj_mean = seq(0.05, 10, length.out = 3),
+    mnt_mean = seq(0.05, 10, length.out = 3),
+    maj_sd = seq(0.01, 10, length.out = 3),
+    mnt_sd = seq(0.01, 10, length.out = 3),
+    p_white = c(0.5, 0.9),
+    p_trt = 0.5,
     n_train = 200,
-    n_test = 100,
-    seed = 20211201
+    n_test = 100
 )
+grd$seed = seq(nrow(grd)) * pi
 
-eval_dgpw(grd[1,])
-eval_dgpw(grd[2,])
-eval_dgpw(grd[3,])
-eval_dgpw(grd[4,])
-
+#res = vapply(seq(nrow(grd)), function(i) as.matrix(eval_dgpw(grd[i, ])), matrix(NA_character_, 2, 4))
+res = lapply(
+    seq(5),
+    function(i) {
+      out = reshape2::melt(eval_dgpw(grd[i, ]))
+      ch = subset(out, metric == "surv.cindex")
+      ibs = subset(out, metric == "surv.graf")
+      ch_diff = as.numeric(abs(subset(ch, variable == "group_1", select = "value") -
+        subset(ch, variable == "group_0", select = "value")))
+      ibs_diff = as.numeric(abs(subset(ibs, variable == "group_1", select = "value") -
+        subset(ibs, variable == "group_0", select = "value")))
+      data.frame(comb = i, Ch = ch_diff, IBS = ibs_diff)
+    }
+)
+res = do.call(rbind, res)
+res
